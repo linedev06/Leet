8f26dd5a1b3c7097f797f0c7f837bda72e18393a
linedev06 <vitorafelismino@gmail.com>
2023-04-01 13:09:08 -0300

    exercicio feito

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Escreva um texto:  ");
+
+Console.WriteLine(
+
+Console.ReadLine()!
+.Replace("A", "4")
+.Replace("a", "4")
+.Replace("E", "3")
+.Replace("e", "3")
+.Replace("I", "1")
+.Replace("i", "1")
+.Replace("L", "1")
+.Replace("l", "1")
+.Replace("O", "0")
+.Replace("o", "0")
+.Replace("T", "7")
+.Replace("t", "7")
+.Replace("S", "5")
+.Replace("s", "5")
+);
+
